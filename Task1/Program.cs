9973142377e5ef27b1265ebe794a50f77f76c58d
Elbrus5642 <satellitprof@gmail.com>
2022-12-03 12:36:55 +0300

    Homework_2 Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите положительное трёхзначное число:");
+string  numberStr = Console.ReadLine() ??"";
+int threeDigitnumber = int.Parse(numberStr);
+int firstDigit = 0;
+int secondDigit = 0;
+
+if (threeDigitnumber >= 100 && threeDigitnumber < 1000)
+{
+    firstDigit = threeDigitnumber / 100;
+        secondDigit = (threeDigitnumber - firstDigit * 100) / 10; 
+    Console.WriteLine($"Вторая цифра трёхзначного числа {threeDigitnumber} -> {secondDigit}"); 
+}
+else
+{
+  Console.WriteLine("Число введено неверно, повторите ввод:");  
+}
