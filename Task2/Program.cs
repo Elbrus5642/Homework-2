1788c2e3fa490a939232436afb06458bd136a1ab
Elbrus5642 <satellitprof@gmail.com>
2022-12-04 15:11:26 +0300

    Homework_2 Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,19 +10,38 @@ string  numberStr = Console.ReadLine() ??"";
 int number = int.Parse(numberStr);
 int resultDiv = number;
 int numberDigit = 0;
-//int firstDigit = 0;
-double stepen = 0;
-//int secondDigit = 0;
-//int thirdnumber = 0;
+int firstDigit = 0;
+int secondDigit = 0;
+int thirdDigit = 0;
+int stepenOne = 0; 
+int stepenTwo = 0;
+int stepenThree = 0;
 
 while (resultDiv > 0)
 {
     resultDiv = resultDiv / 10;
     numberDigit ++; 
 }
+if (numberDigit > 2)
+{
 
 Console.WriteLine($"Разрядность числа {number} -> {numberDigit}");
+stepenOne = Convert.ToInt32(Math.Pow (10,numberDigit - 1));
+Console.WriteLine($"Делитель числа {number}  для определения первой цифры -> {stepenOne}");
+firstDigit = number / stepenOne;
+Console.WriteLine($"Первая цифра числа {number} -> {firstDigit}");
 
-stepen = Math.Pow (10,numberDigit - 1);
-Console.WriteLine($"Возведение в степень {numberDigit} -> {stepen}");
-// Console.WriteLine($"Первая цифра числа {number} -> {firstDigit}");
+stepenTwo = Convert.ToInt32(Math.Pow (10,numberDigit - 2));
+Console.WriteLine($"Делитель числа {number}  для определения второй цифры -> {stepenTwo}");
+secondDigit = Convert.ToInt32((number - (firstDigit * stepenOne)) / stepenTwo);
+Console.WriteLine($"Вторая цифра числа {number} -> {secondDigit}"); 
+
+stepenThree = Convert.ToInt32(Math.Pow (10,numberDigit - 3));
+Console.WriteLine($"Делитель числа {number}  для определения третьей цифры -> {stepenThree}");
+thirdDigit = (number - firstDigit * (stepenOne - 1) - secondDigit * (stepenTwo - 2))/stepenThree;
+Console.WriteLine($"Третья цифра числа {number} -> {thirdDigit}");
+}
+else
+{
+  Console.WriteLine($"Число {number} состоит менее чем из трёх разрядов, третьей цифры нет");  
+}
